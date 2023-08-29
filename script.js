814133ffcb0b0bcc20648d86bf1dc87a9bb52474
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-29 15:00:31 +0100

    added userinput to list

--- script.js
+++ script.js
@@ -5,16 +5,29 @@
 
 const handleSubmit = (event) => {
     event.preventDefault();
+
+    const newToDoInput = document.querySelector('#new-todo');
+    const newToDoText = newToDoInput.value.trim();
+
+    if (newToDoText !== "") {
+        const newListItem = document.createElement('li');
+        newListItem.innerText = newToDoText;
+    }
+
     const newToDo = toDoForm.target;
     console.log(newToDo);
     const newListItem = document.createElement('li');
-    newListItem.innerText = newToDo;
+    newListItem.innerText = newToDoText;
 
     const list = document.querySelector('#list');
     list.appendChild(newListItem);
+
+    
 
 }
 const toDoForm = document.querySelector('#todo-form');
 console.log(toDoForm);
 
 toDoForm.addEventListener('submit', handleSubmit);
+
+const toDoList = document.querySelector('#list');
